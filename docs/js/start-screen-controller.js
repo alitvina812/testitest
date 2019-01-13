95691ee27313f9dc2b53481724ea5e6195d4ca06
Anastasiia Litvina <al@click-solutions.de>
2019-01-13 20:15:39 +0100

    edit

--- docs/js/start-screen-controller.js
+++ docs/js/start-screen-controller.js
@@ -7,7 +7,7 @@ class StartScreenController extends ViewController {
     }
 
     display() {
-        this.setContentHeight();
+        // this.setContentHeight();
         this.elements.mainEl = document.querySelector("main");
         this.elements.mainEl.appendChild(document.getElementById("start-template").content.cloneNode(true).firstElementChild);
     }
--- docs/js/view-controller.js
+++ docs/js/view-controller.js
@@ -13,8 +13,8 @@ class ViewController {
     }
 
     setContentHeight() {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        // let vh = window.innerHeight * 0.01;
+        // document.documentElement.style.setProperty('--vh', `${vh}px`);
     }
 }
 
